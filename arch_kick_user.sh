1fd18d1ea89f7ad227f45490175d42e91f090d2c
Dr-42 <royspandan1@gmail.com>
2024-04-27 10:51:29 +0530

    add cd

--- arch_kick_user.sh
+++ arch_kick_user.sh
@@ -1,14 +1,18 @@
 rustup install stable
 cargo install offflix
 cargo install builder_cpp
+cd $HOME
 git clone https://github.com/Dr-42/dotfiles
 cp -r dotfiles/.config ~/
 cp -r dotfiles/.zshrc ~/
 mkdir ~/Projects
-git clone https://github.com/Dr-42/time_scheduler ~/Projects/time_scheduler
-git clone https://github.com/Dr-42/AnalogueGrandClock ~/Projects/AnalogueGrandClock
+cd ~/Projects
+git clone https://github.com/Dr-42/time_scheduler
+git clone https://github.com/Dr-42/AnalogueGrandClock
+cd $HOME
 rm ~/Projects/AnalogueGrandClock/.git -rf
 mv ~/Projects/AnalogueGrandClock ~/.local/share/plasma/plasmoids/org.dr42.analoguegrandclock
 flatpak install flathub com.usebottles.bottles
 mkdir Vaults
+cd ~/Vaults
 git clone https://github.com/Dr-42/MedNotes ~/Vaults/MedNotes
